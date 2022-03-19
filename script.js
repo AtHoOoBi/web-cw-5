c410201161cc2528483d3d54bc0f71513342073a
AtHoOoBi <alkingathbi@gmail.com>
2022-03-19 18:01:48 +0300

    KiNG

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ const style = "color:blue";
 console.log(4 * 4, style);
 console.log("أنا في الصف العاشر ");
 // [جافاسكربت ليست جافا]
+
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+budget += 30;
+console.log(budget > 30 && budget < 80);
+
+let grade = prompt("أدخل درجتك");
+A = grade >= 90 && grade < 100;
+S = grade >= 80;
+D = grade >= 70;
+F = grade >= 60;
+H = grade >= 50;
+G = grade <= 50;
+switch (grade) {
+  case "A":
+    console.log("لقد حصلت على امتياز 🥳");
+  case "S":
+    console.log("لقد حصلت على جيد جداً🤩");
+  case "D":
+    console.log("لقد حصلت على جيد🙂");
+  case "F":
+    console.log("لقد حصلت على مقبول😕");
+  case "H":
+    console.log("لقد حصلت على ضعيف☹️");
+  case "G":
+    console.log("راسب");
+}
